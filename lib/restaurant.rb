c147f524fd0dabb52ddfe8eb9e3e744cb62f653d
Sage Lee <sagemcklee@gmail.com>
2020-03-18 14:00:26 -0600

    Add tests

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -1,1 +1,24 @@
+class Restaurant
+  attr_reader :opening_time,
+              :name
 
+  def initialize (opening_time, name)
+    @opening_time = opening_time
+    @name = name
+    @dishes = []
+  end
+
+def dishes
+  @dishes
+end
+
+def closing_time (hours)
+  storehours = @opening_time.to_i + hours
+  storehours.to_s + ":00"
+end
+
+dishes = []
+def add_dish (dish)
+  dishes << dish
+end
+end
--- test/restaurant_test.rb
+++ test/restaurant_test.rb
@@ -11,21 +11,21 @@ class RestaurantTest < Minitest::Test
   end
 
   def test_it_has_opening_time
-    skip
+
     restaurant = Restaurant.new('10:00', 'Fuel Cafe')
 
     assert_equal '10:00', restaurant.opening_time
   end
 
   def test_it_has_name
-    skip
+
     restaurant = Restaurant.new('10:00', 'Fuel Cafe')
 
     assert_equal 'Fuel Cafe', restaurant.name
   end
 
   def test_it_has_dishes
-    skip
+
     restaurant = Restaurant.new('10:00', 'Fuel Cafe')
 
     assert_equal [], restaurant.dishes
@@ -33,7 +33,7 @@ class RestaurantTest < Minitest::Test
 
   #Iteration 2 Tests:
   def test_it_has_closing_time
-    skip
+
     restaurant1 = Restaurant.new('10:00', 'Fuel Cafe')
     restaurant2 = Restaurant.new('16:00', 'Il Poggio')
 
@@ -42,7 +42,7 @@ class RestaurantTest < Minitest::Test
   end
 
   def test_it_can_add_dishes
-    skip
+    
     restaurant = Restaurant.new('16:00', 'Il Poggio')
 
     restaurant.add_dish('Burrata')
